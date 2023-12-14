b30b9cb801a98a6350a88007131f3f0dcfb549d7
caleb.moses <calebjdmoses@gmail.com>
2023-12-14 04:31:13 -0500

    Update prepare emails

--- src/prepare_emails.sh
+++ src/prepare_emails.sh
@@ -1,10 +1,18 @@
 #! /bin/bash
 
+cleanup() {
+    echo "Cleaning up..."
+    rm -rf data/enron_mail_20150507.tar.gz data/maildir
+}
+
+trap cleanup SIGINT
+
 echo "Downloading enron email dataset"
 wget https://www.cs.cmu.edu/~enron/enron_mail_20150507.tar.gz -O data/enron_mail_20150507.tar.gz
 echo "Unzipping..."
 tar -xf data/enron_mail_20150507.tar.gz -C data/
 echo "Converting raw emails to parquet"
 python code/convert_to_parquet.py --input_dir data/maildir --output data/enron_emails.parquet
-echo "Deleting raw files"
-rm -rf data/enron_mail_20150507.tar.gz data/maildir
+
+# Call cleanup at the end of the script
+cleanup
