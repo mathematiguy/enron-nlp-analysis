edf390051090816c9816d300bc376cc7ec6fabc5
caleb.moses <calebjdmoses@gmail.com>
2023-12-17 05:15:22 -0500

    Add dataset masking

---
+++ code/mask_datasets.py
@@ -0,0 +1,121 @@
+import click
+import pandas as pd
+import re
+import spacy
+import logging
+from tqdm import tqdm
+from utils import parallel_batch_apply
+
+# Set up basic configuration for logging
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+
+def load_nlp_model():
+    """
+    Loads the spaCy NLP model and sets up a custom EntityRuler with predefined patterns.
+
+    Returns:
+        Loaded spaCy NLP model with custom EntityRuler.
+    """
+    # Loading spaCy NLP model and setting up EntityRuler...
+    nlp = spacy.load("en_core_web_sm")
+    nlp.max_length = 2000000  # Increase the character limit to 2 million characters, for example
+    name_ruler = nlp.add_pipe("entity_ruler")
+    patterns = [
+        {"label": "PERSON", "pattern": [{"lower": "delainey"}]},
+        {"label": "PERSON", "pattern": [{"lower": "jmf"}]},
+        {"label": "PERSON", "pattern": [{"lower": "dave"}]},
+        {"label": "PERSON", "pattern": [{"lower": "forney"}]},
+        {"label": "PERSON", "pattern": [{"lower": "lloyd"}]},
+        {"label": "PERSON", "pattern": [{"lower": "phillip"}]},
+        {"label": "PERSON", "pattern": [{"lower": "tj"}]},
+        {"label": "ORG", "pattern": [{"lower": "ercot"}]},
+    ]
+    name_ruler.add_patterns(patterns)
+    return nlp
+
+
+def change_ents(doc):
+    """
+    Replaces named entities in a document based on predefined replacements.
+
+    Args:
+        doc (spaCy Doc): A spaCy document object.
+
+    Returns:
+        str: The text of the document with entities replaced.
+    """
+    # Entity replacements mapping
+    ent_replacements = {
+        "PERSON": "Steve",
+        "ORG": "Apple",
+        "GPE": "Cupertino",
+    }
+
+    # Compile regex patterns for entity replacements
+    regex_ent_replacements = {key: "" for key in ent_replacements}
+    for ent in doc.ents:
+        if ent.label_ in ent_replacements:
+            text = re.escape(re.sub(r"\(.*|\).*|\+.*", "", ent.text))
+            regex_ent_replacements[ent.label_] += f"|{text}"
+    regex_ent_replacements = {
+        key: val[1:] for key, val in regex_ent_replacements.items() if val
+    }
+
+    # Replace entities in the text
+    new_text = doc.text
+    for ent_label, regex in regex_ent_replacements.items():
+        try:
+            new_text = re.sub(
+                regex, ent_replacements[ent_label], new_text, flags=re.IGNORECASE
+            )
+        except re.error as e:
+            logging.error(f"Regex error with pattern {regex}: {e}")
+            continue
+    new_text = re.sub(r"[ \n\t]+", " ", new_text)
+    return new_text.strip()
+
+
+def process_texts(batch):
+    """
+    Processes a batch of texts using the spaCy NLP model and replaces named entities.
+
+    Args:
+        batch (list): A list of texts to process.
+
+    Returns:
+        list: A list of processed texts with entities replaced.
+    """
+    nlp = load_nlp_model()
+    return [change_ents(text) for text in nlp.pipe(batch)]
+
+
+@click.command()
+@click.option(
+    "--email_file", type=click.Path(exists=True), help="Path to the input email file."
+)
+@click.option("--output", type=click.Path(), help="Path for the output file.")
+def main(email_file, output):
+    """
+    Main function to process emails from a file and save the results to an output file.
+
+    Args:
+        email_file (str): Path to the input email file.
+        output (str): Path for the output file.
+    """
+    logging.info(f"Reading email data from {email_file}...")
+    df = pd.read_csv(email_file, index_col="Original Index")
+
+    logging.info("Starting email processing...")
+    replaced_emails = parallel_batch_apply(df["Email"], process_texts, batch_size=len(df["Email"]) // 16 // 10, cores=16)
+
+    df["Masked Email"] = replaced_emails
+    logging.info(f"Saving masked emails to {output}...")
+    df.to_csv(output)
+    logging.info("Masking complete. Output saved.")
+
+
+if __name__ == "__main__":
+    main()
--- code/utils.py
+++ code/utils.py
@@ -1,3 +1,4 @@
+import math
 import concurrent.futures
 import multiprocessing
 import pandas as pd
@@ -10,6 +11,23 @@ def process_chunk(chunk, func):
 
 
 def parallel_apply(input_list, func, cores=None, n_series=0):
+    """
+    Applies a function to each item of a list or pandas Series in parallel, optionally processing the first n items sequentially.
+
+    Args:
+        input_list (list or pd.Series): The list or pandas Series of items to which the function will be applied.
+        func (function): The function to apply to each item. This function should take a single argument and return a result.
+        cores (int, optional): The number of CPU cores to use for parallel processing. If None, uses the number of available cores.
+        n_series (int, optional): The number of items to process sequentially before parallel processing. Default is 0.
+
+    Returns:
+        list or pd.Series: A list or pandas Series (matching the input type) of results with the function applied to each item.
+
+    This function processes the first `n_series` items of the input list/series sequentially, and then processes the 
+    remaining items in parallel using multiprocessing. The list is divided into chunks, each processed by a separate 
+    process. If the input is a pandas Series, the output will also be a Series with the same index.
+    """
+    
     if cores is None:
         cores = multiprocessing.cpu_count()
 
@@ -45,5 +63,45 @@ def parallel_apply(input_list, func, cores=None, n_series=0):
     # Convert back to Series if the input was a Series
     if is_series:
         return pd.Series(results, index=index)
+
+    return results
+
+
+
+def parallel_batch_apply(input_list, batch_func, batch_size=None, cores=None):
+    """
+    Applies a function to batches of items in a list in parallel.
+
+    Args:
+        input_list (list): The list of items to process.
+        batch_func (function): The function to apply to each batch. This function should take a list (batch) and return a list.
+        batch_size (int, optional): The size of each batch. If None, it's calculated based on the number of cores.
+        cores (int, optional): The number of CPU cores to use. Defaults to the number of available cores.
+
+    Returns:
+        list: The combined results from processing each batch.
+    """
+    if cores is None:
+        cores = multiprocessing.cpu_count()
+
+    # Calculate the batch size
+    if batch_size is None:
+        batch_size = math.ceil(len(input_list) / cores)
+
+    # Split the list into batches
+    batches = [
+        input_list[i : i + batch_size] for i in range(0, len(input_list), batch_size)
+    ]
+
+    results = []
+    with concurrent.futures.ProcessPoolExecutor(max_workers=cores) as executor:
+        # Submit all batches for processing
+        future_to_batch = {
+            executor.submit(batch_func, batch): i for i, batch in enumerate(batches)
+        }
+
+        # Collect results as batches are completed
+        for future in tqdm(concurrent.futures.as_completed(future_to_batch), total=len(batches), desc="Processing Batches"):
+            results.extend(future.result())
 
     return results
